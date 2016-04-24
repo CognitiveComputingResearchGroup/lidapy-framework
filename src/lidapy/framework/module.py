4aa6a99e82a27c24fa54c604e32cb6dde591aad1
Sean Kugele <seankugele@gmail.com>
2016-04-24 12:40:15 -0500

    Updates for publishers and subscribers.

--- src/lidapy/framework/module.py
+++ src/lidapy/framework/module.py
@@ -10,7 +10,9 @@ class FrameworkModule(object):
         
     def __init__(self, name):
         self.name = name
+
         self._publishers = {}
+        self._receivedMsgs = {}
 
         comm.initialize(self.name)
 
@@ -20,13 +22,33 @@ class FrameworkModule(object):
         self._publishers[topic] = comm.getPublisher(topic, msg_type, queue_size = queue_size)
         return
     
-    def _addSubscriber(self, topic, msg_type, callback, callback_args=[]):
-        comm.registerSubscriber(topic, msg_type, callback, callback_args)
+    def _addSubscriber(self, topic, msg_type, callback=None, callback_args={}):
+        if callback is None:
+            callback = self._receiveMsg
+
+        sub_args = {"topic" : topic}
+        sub_args.update(callback_args)
+
+        comm.registerSubscriber(topic, msg_type, callback, sub_args)
+
+        self._receivedMsgs[topic] = []
+
         return
 
     def _publish(self, topic, msg):
         comm.publishMessage(self._publishers[topic], msg)
         return
+
+    def _receiveMsg(self, msg, args):
+        topic = args["topic"]
+        if topic is not None:
+            msgQueue = self._receivedMsgs[topic]
+            if msgQueue is not None:
+                msgQueue[topic].append(msg)
+        return
+
+    def getNextMsg(self, topic):
+        return self._receivedMsgs[topic].pop()
 
     def run(self, pubRate):
         comm.run(pubRate)
--- src/lidapy/module/sensory_memory.py
+++ src/lidapy/module/sensory_memory.py
@@ -5,14 +5,32 @@ Created on Apr 20, 2016
 @author: Sean Kugele
 '''
 from lidapy.framework.module import FrameworkModule
+from std_msgs.msg import String
 
 
 class SensoryMemoryModule(FrameworkModule):
 
     def __init__(self):
-        FrameworkModule.__init__(self, "SensoryMemoryModule")
+        super(SensoryMemoryModule, self).__init__("SensoryMemoryModule")
+
+        self.addPublishers()
+        self.addSubscribers()
+
         return
 
+    def addPublishers(self):
+        pubs = [{"topic": "/lida/sensory_memory", "msg_type" : String}]
+        for pub in pubs:
+            super(SensoryMemoryModule, self)._addPublisher(pub["topic"], pub["msg_type"])
+
+        return
+
+    def addSubscribers(self):
+        subs = [{"topic": "/lida/environment", "msg_type" : String}]
+        for sub in subs:
+            super(SensoryMemoryModule, self)._addSubscriber(sub["topic"], sub["msg_type"])
+
+        return
 
 
 if __name__ == '__main__':
