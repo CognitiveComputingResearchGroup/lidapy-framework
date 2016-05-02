b80a776e45ac8c6ceaa5edbd4209ed07046c5612
Sean Kugele <seankugele@gmail.com>
2016-05-01 22:12:26 -0500

    Added logic to read agent configuration filepath from environment.  Raises an exception if unable to find or open the agent configuration file.

--- src/lidapy/framework/agent.py
+++ src/lidapy/framework/agent.py
@@ -1,9 +1,15 @@
 from ConfigParser import SafeConfigParser
 from lidapy.util import logger
 from lidapy.util.comm import ParameterService
+from os import getenv
 
 
 class AgentConfig(object):
+    # Environment variable for agent configuration file
+    agent_config_env_var = "LIDAPY_AGENT_CONFIG"
+
+    # Default filepath for agent configuration file if not set in environment
+    default_agent_config_filepath = "configs/agent.conf"
 
     # shared configuration dictionary containing key/value pairs
     # for each section in the configuration file
@@ -20,9 +26,27 @@ class AgentConfig(object):
 
     _initialized = False
 
-    def __init__(self, config_file="configs/agent.conf"):
+    def __init__(self, config_file):
         if not AgentConfig._initialized:
             self._load_config(config_file)
+
+    def _find_config_file(self, config_file):
+
+        config_filepath = getenv(AgentConfig.agent_config_env_var)
+        if not config_filepath:
+            logger.warn("Agent configuration file not set in environment variable {}, using default {}".format(
+                AgentConfig.agent_config_env_var,
+                AgentConfig.default_agent_config_filepath))
+
+            # using default
+            config_filepath = AgentConfig.default_agent_config_filepath
+
+        try:
+            # Test if file exists and has read permissions
+            with open(config_filepath) as file:
+                pass
+        except IOError as e:
+            raise IOError("Unable to open agent config file [{}]".format(config_filepath))
 
     def _load_config(self, config_file):
         self._load_file_config(config_file)
@@ -53,13 +77,16 @@ class AgentConfig(object):
     def get_param(self, param_type, param_name, default_value):
         param_value = default_value
         if AgentConfig._param_service is None:
-            logger.debug("Retrieving parameter [type = {}, name = {}] from file config".format(param_type, param_name, default_value))
+            logger.debug("Retrieving parameter [type = {}, name = {}] from file config".format(param_type, param_name,
+                                                                                               default_value))
 
             print AgentConfig._file_config
             param_value = AgentConfig._file_config[param_type][param_name]
         else:
-            logger.debug("Retrieving parameter [type = {}, name = {}] from parameter service".format(param_type, param_name, default_value))
-            
+            logger.debug(
+                "Retrieving parameter [type = {}, name = {}] from parameter service".format(param_type, param_name,
+                                                                                            default_value))
+
             param_value = AgentConfig._param_service.get_param(param_type,
                                                                param_name,
                                                                default_value)
