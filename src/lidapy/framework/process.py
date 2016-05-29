ccc9bd758850df3e3efebafb6d455e9189c2f3da
Sean Kugele <seankugele@gmail.com>
2016-05-29 17:04:25 -0500

    corrected "name" keyword arg to Process class.

--- src/lidapy/framework/process.py
+++ src/lidapy/framework/process.py
@@ -6,21 +6,21 @@ from lidapy.util import comm, logger
 
 
 class FrameworkProcess(Process):
-    def __init__(self, process_name, **kwargs):
-        super(FrameworkProcess, self).__init__(process_name)
+    def __init__(self, name, **kwargs):
+        super(FrameworkProcess, self).__init__(name=name)
 
-        self.process_name = process_name
+        self.name = name
         self.log_level = kwargs.get("log_level")
 
     def run(self):
-        logger.info("Beginning execution for process = {} [pid = {}]".format(self.process_name, os.getpid()))
+        logger.info("Beginning execution for process = {} [pid = {}]".format(self.name, os.getpid()))
 
         self.initialize()
 
         try:
 
             while not comm.shutting_down():
-                rate_in_hz = int(AgentConfig().get_type_or_global_param(self.process_name, "rate_in_hz", 100))
+                rate_in_hz = int(AgentConfig().get_type_or_global_param(self.name, "rate_in_hz", 100))
                 self.advance()
                 comm.wait(rate_in_hz)
 
@@ -30,7 +30,7 @@ class FrameworkProcess(Process):
     def initialize(self):
 
         # Register this process with communication infrastructure
-        comm.initialize(self.process_name, log_level=self.log_level)
+        comm.initialize(self.name, log_level=self.log_level)
 
     # Must be overridden
     def advance(self):
