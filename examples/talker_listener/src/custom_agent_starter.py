666a0071f0bc38f7e274f801f041ea6a0f172193
Sean Kugele <seankugele@gmail.com>
2016-08-08 04:13:16 +0000

    Updates to talker_listener example.

--- examples/talker_listener/src/custom_agent_starter.py
+++ examples/talker_listener/src/custom_agent_starter.py
@@ -5,47 +5,80 @@ from lidapy.framework.module import FrameworkModule
 from lidapy.framework.msg import FrameworkTopic
 from lidapy.util import logger
 
+# Standard message supported by ROS (Robot Operating System)
 from std_msgs.msg import String
 
+# Module names.  These are used for the names of the ROS nodes, and for
+# registering the modules with agent_starter.
+TALKER_MODULE_NAME = "talker"
+LISTENER_MODULE_NAME = "listener"
 
-TOPIC = FrameworkTopic("lida/comm", String)
+# Topics used in this example
+TALKER_LISTENER_TOPIC = FrameworkTopic("lida/talker_listener_example", String, use_serializer=True)
 
 
 class TalkerModule(FrameworkModule):
     def __init__(self, **kwargs):
-        super(TalkerModule, self).__init__("talker", **kwargs)
-        super(TalkerModule, self).add_publisher(TOPIC)
+        super(TalkerModule, self).__init__(TALKER_MODULE_NAME, **kwargs)
 
+    @classmethod
+    def get_module_name(cls):
+        return TALKER_MODULE_NAME
+
+    def add_publishers(self):
+        super(TalkerModule, self).add_publisher(TALKER_LISTENER_TOPIC)
+
+    # The call method is executed automatically every (1/rate_in_hz) seconds.  The
+    # rate_in_hz parameter is defined in the agent.conf configuration file.
     def call(self):
-        next_msg = self.create_next_msg()
-        logger.info("Sending message: {}".format(next_msg))
-        self.publishers[TOPIC.topic_name].publish(next_msg)
 
-    def create_next_msg(self):
-        next_msg = self.config.get_param("talker", "message")
-        return next_msg
+        # Read message text from agent.conf
+        msg = self.config.get_param(TALKER_MODULE_NAME, "message")
+
+        # Write the message to the logger (/rosout)
+        logger.info("Sending message: {}".format(msg))
+
+        # Publish the message over the talker/listener topic
+        super(TalkerModule, self).publish(TALKER_LISTENER_TOPIC, msg)
 
 
 class ListenerModule(FrameworkModule):
     def __init__(self, **kwargs):
-        super(ListenerModule, self).__init__("listener", **kwargs)
-        super(ListenerModule, self).add_subscriber(TOPIC)
+        super(ListenerModule, self).__init__(LISTENER_MODULE_NAME, **kwargs)
 
+    @classmethod
+    def get_module_name(cls):
+        return LISTENER_MODULE_NAME
+
+    def add_subscribers(self):
+        super(ListenerModule, self).add_subscriber(TALKER_LISTENER_TOPIC)
+
+    # The call method is executed automatically every (1/rate_in_hz) seconds.  The
+    # rate_in_hz parameter is defined in the agent.conf configuration file.
     def call(self):
-        msg = self.get_next_msg(TOPIC.topic_name)
+
+        # Pulls a recently received message from the message queue that is
+        # maintained in FrameworkModule (parent class)
+        msg = super(ListenerModule, self).get_next_msg(TALKER_LISTENER_TOPIC)
 
         if msg is not None:
-            logger.info("Receiving message: {}".format(msg))
+            logger.info("Received message: {}".format(msg))
 
 
 if __name__ == '__main__':
 
     try:
+        # Execute custom_agent_starter.py with "-h" option for help on usage.
         starter = AgentStarter()
 
-        starter.add_module("talker", TalkerModule)
-        starter.add_module("listener", ListenerModule)
+        # Registers TalkerModule and ListenerModule with the agent_starter.  After
+        # registration, these modules can be invoked from a roslaunch file
+        # (or command-line) using "-m <module name>" (see agent.launch for more details)
+        starter.add_module(TalkerModule)
+        starter.add_module(ListenerModule)
 
+        # Launches a ROS node for the specified module.  Note that only one ROS node
+        # will be launched per invocation of custom_agent_starter.py.
         starter.start()
 
     except Exception as e:
