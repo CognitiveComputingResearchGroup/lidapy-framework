12138d210a2b9bee2ac1d070df8b57590afc26cc
Sean Kugele <seankugele@gmail.com>
2016-12-10 17:34:26 +0000

    Added files in support of continuous integration using Travis CI.

---
+++ scripts/setup_test.bash
@@ -0,0 +1,6 @@
+# Setup ROS environment variables
+source /opt/ros/jade/setup.bash
+
+LIDAPY_FRAMEWORK=$(find $HOME -type d -name lidapy-framework 2>/dev/null)
+
+export PYTHONPATH=$PYTHONPATH:$LIDAPY_FRAMEWORK/src:$LIDAPY_FRAMEWORK/test
