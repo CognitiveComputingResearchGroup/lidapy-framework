685b059a3f474d2b7b4af1ca05fe953bc18735a8
Sean Kugele <seankugele@gmail.com>
2016-12-10 19:47:24 +0000

    Adding line to display the PYTHONPATH

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,5 +7,7 @@ source $LIDAPY_FRAMEWORK/scripts/setup_test.bash
 # Assumes script is being run from the scripts directory
 cd $LIDAPY_FRAMEWORK/test/lidapy
 
+echo "PYTHONPATH: " $PYTHONPATH
+
 # Uses unittest autodiscovery to find and run all unittests under the test directory
 python -m unittest discover --pattern '*.py'
