a16833a60603a3bfe315e66f5902cf6b6caba19f
Sean Kugele <seankugele@gmail.com>
2016-12-10 18:50:38 +0000

    In desperation I changed to ros full desktop install.

--- scripts/install_test_dependencies.sh
+++ scripts/install_test_dependencies.sh
@@ -2,12 +2,11 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 sudo apt-get update -qq
-sudo apt-get install ros-jade-ros-base
+sudo apt-get install ros-jade-desktop-full
 sudo rosdep init
 rosdep -y update
 sudo apt-get install gazebo5
 sudo apt-get install ros-jade-gazebo-ros-pkgs
-pip install nose
-pip install pyyaml
-pip install rospkg
-sudo apt-get install -y python-rosinstall
+#pip install nose
+#pip install pyyaml
+#pip install rospkg
