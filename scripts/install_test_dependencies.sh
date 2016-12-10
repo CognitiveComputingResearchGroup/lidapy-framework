e1c0abcab22777a75c456edc7432715928d1951a
Sean Kugele <seankugele@gmail.com>
2016-12-10 18:57:17 +0000

    More changes.

--- scripts/install_test_dependencies.sh
+++ scripts/install_test_dependencies.sh
@@ -3,6 +3,7 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 sudo apt-get update -qq
 sudo apt-get install ros-jade-desktop-full
+sudo apt-get -qq install build-essential python-dev
 sudo rosdep init
 rosdep -y update
 sudo apt-get install gazebo5
