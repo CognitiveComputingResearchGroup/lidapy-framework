1ddb2322ce3629ac7557e5f904792f81a5556ee7
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2016-12-10 17:04:02 -0600

    change back to discover test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,7 +7,9 @@ LIDAPY_FRAMEWORK=$(find $HOME -type d -name lidapy-framework 2>/dev/null)
 # Assumes script is being run from the scripts directory
 cd $LIDAPY_FRAMEWORK/test/lidapy
 
+pwd
+
 echo "PYTHONPATH: " $PYTHONPATH
 
 # Uses unittest autodiscovery to find and run all unittests under the test directory
-python -m unittest framework.strategy
+python -m unittest discover --pattern '*.py'
