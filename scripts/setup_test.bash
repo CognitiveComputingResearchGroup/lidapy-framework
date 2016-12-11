9ff6eed928e0b473650a0a651479bee948564ad4
Sean Kugele <seankugele@gmail.com>
2016-12-11 02:38:03 +0000

    Added /usr/lib/python2.7/dist-packages to PYTHONPATH in test env

--- scripts/setup_test.bash
+++ scripts/setup_test.bash
@@ -3,4 +3,4 @@ source /opt/ros/jade/setup.bash
 
 LIDAPY_FRAMEWORK=$(find $HOME -type d -name lidapy-framework 2>/dev/null)
 
-export PYTHONPATH=$PYTHONPATH:$LIDAPY_FRAMEWORK/src
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages:$LIDAPY_FRAMEWORK/src
