85fccf9815821af3a471f67553d9b9fdbb5994ea
Sean Kugele <seankugele@gmail.com>
2016-12-11 02:56:12 +0000

    Removed unneeded commands and scripts for test setup.  Cleaned up Travis configuration.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,18 +1,9 @@
 #! /bin/bash
 
-source ~/.lidapy/setup.bash
-
 LIDAPY_FRAMEWORK=$(find $HOME -type d -name lidapy-framework 2>/dev/null)
 
 # Assumes script is being run from the scripts directory
 cd $LIDAPY_FRAMEWORK/test/lidapy
-
-pwd
-
-echo "PYTHONPATH: " $PYTHONPATH
-
-echo "Sys Paths from Python"
-python ../../scripts/test_syspath.py
 
 # Uses unittest autodiscovery to find and run all unittests under the test directory
 python -m unittest discover --pattern '*.py' -v
